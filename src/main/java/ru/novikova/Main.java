e5b208e40d171434f423d068dded8340436091f6
alexaNovikova <alexadubinina@mail.ru>
2024-06-20 11:21:33 +0700

    Создан метод updateStrings

--- src/main/java/ru/novikova/Main.java
+++ src/main/java/ru/novikova/Main.java
@@ -1,7 +1,39 @@
 package ru.novikova;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите размер массива строк");
+        int n = scanner.nextInt();
+        String[] strings = new String[n];
+        scanner.nextLine();
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println("Введите строку " + i + " и нажмите Enter");
+            strings[i] = scanner.nextLine();
+        }
+        scanner.close();
+        String[] updated = updateStrings(strings);
+        System.out.println(Arrays.toString(updated));
+    }
+
+    private static String[] updateStrings(String[] strings) {
+        int size = 0;
+        for (String string : strings) {
+            if (string.length() <= 3) {
+                size++;
+            }
+        }
+        int counter = 0;
+        String[] result = new String[size];
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() <= 3) {
+                result[counter] = strings[i];
+                counter++;
+            }
+        }
+        return result;
     }
 }
